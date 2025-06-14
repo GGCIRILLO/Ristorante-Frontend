72e73001df19776802357a7f64313c875ebf0b0c
Luigi Cirillo <luigicirillo23@gmail.com>
2025-06-14 18:12:22 +0200

    feat: update index.html title and remove unused App.css; enhance HomePage layout and RoleCard styles

--- src/components/RoleCard.tsx
+++ src/components/RoleCard.tsx
@@ -9,21 +9,20 @@ type RoleCardProps = {
 
 const RoleCard: FC<RoleCardProps> = ({ iconSrc, label, route }) => {
   const navigate = useNavigate();
-  console.log("RoleCard rendered with route:", route);
 
   return (
     <button
       onClick={() => {
         navigate(route);
       }}
-      className="w-64 h-44 bg-white rounded-xl items-center justify-between p-8 shadow-md flex flex-col transition-transform hover:scale-105 cursor-pointer hover:bg-gray-50 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      className="w-lg h-56 bg-white rounded-xl items-center justify-center p-8 shadow-md flex flex-col transition-transform hover:scale-105 cursor-pointer hover:bg-gray-50 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
     >
       {/* icona compatta, non si allarga più del dovuto */}
-      <div className="flex items-center justify-center w-16 h-16 mb-4">
+      <div className="flex items-center justify-center">
         <img
           src={iconSrc}
           alt=""
-          className="w-10 h-10 object-contain flex-shrink-0"
+          className="w-14 h-14 object-contain flex-shrink-0 mb-8"
         />
       </div>
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import RoleCard from "../components/RoleCard";
 import { Icons } from "../assets";
 
 const HomePage: FC = () => (
-  <div className="min-h-screen flex-1">
+  <div className="min-h-screen flex flex-col bg-blu-100">
     {/* Barra superiore */}
     <header className="top-0 z-10 h-16 bg-[#0080c8] flex items-center shadow-md px-4">
       <h1 className="ml-4 text-xl font-bold text-white">GESTIONE RISTORANTE</h1>
@@ -16,7 +16,7 @@ const HomePage: FC = () => (
       </h2>
 
       {/* Griglia 2×2 con padding esterno e spaziatura interna maggiore */}
-      <div className="grid grid-cols-2 gap-16 p-8 mx-auto max-w-4xl">
+      <div className="grid grid-cols-2 gap-8 p-8 mx-auto max-w-6xl">
         <RoleCard iconSrc={Icons.person} label="Cameriere" route="/cameriere" />
         <RoleCard iconSrc={Icons.restaurantMenu} label="Cuoco" route="/cuoco" />
         <RoleCard iconSrc={Icons.payment} label="Cassiere" route="/cassiere" />
